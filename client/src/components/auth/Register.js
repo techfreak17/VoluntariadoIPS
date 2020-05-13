d41e597dd0d1d58e225306992c715a9154f08ccc
César Augusto Fonseca Fontinha <techfreak.thecaff@gmail.com>
2020-05-13 22:17:01 +0100

    Finished base work

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions";
+import classnames from "classnames";
 
 class Register extends Component {
     constructor() {
@@ -15,11 +18,29 @@ class Register extends Component {
             errors: {}
         };
     }
+
+    componentDidMount() {
+        // If logged in and user navigates to Register page, should redirect them to dashboard
+        if (this.props.auth.isAuthenticated) {
+          this.props.history.push("/dashboard");
+        }
+      }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({
+                errors: nextProps.errors
+            });
+        }
+    }
+
     onChange = e => {
         this.setState({ [e.target.id]: e.target.value });
     };
+
     onSubmit = e => {
         e.preventDefault();
+
         const newUser = {
             number: this.state.number,
             name: this.state.name,
@@ -28,32 +49,34 @@ class Register extends Component {
             password: this.state.password,
             password2: this.state.password2
         };
-        console.log(newUser);
+
+        this.props.registerUser(newUser, this.props.history);
     };
+
     render() {
         const { errors } = this.state;
+
         return (
             <div className="container">
                 <div className="row">
                     <div className="col s8 offset-s2">
                         <Link to="/" className="btn-flat waves-effect">
-                            <i className="material-icons left">keyboard_backspace</i>
+                            <i className="material-icons left">keyboard_backspace</i> 
                             Back to home
                         </Link>
-
+                        
                         <div className="col s12" style={{ paddingLeft: "11.250px" }}>
                             <h4>
-                                <b>Register</b>
+                                <b>Register</b> 
                                 below
                             </h4>
-
                             <p className="grey-text text-darken-1">
                                 Already have an account? <Link to="/login">Log in</Link>
                             </p>
-
                         </div>
-
+                        
                         <form noValidate onSubmit={this.onSubmit}>
+                            
                             <div className="input-field col s12">
                                 <input
                                     onChange={this.onChange}
@@ -61,9 +84,14 @@ class Register extends Component {
                                     error={errors.number}
                                     id="number"
                                     type="number"
+                                    className={classnames("", {
+                                        invalid: errors.number
+                                    })}
                                 />
-                                <label htmlFor="number">number</label>
+                                <label htmlFor="number">Número</label>
+                                <span className="red-text">{errors.number}</span>
                             </div>
+
                             <div className="input-field col s12">
                                 <input
                                     onChange={this.onChange}
@@ -71,17 +99,29 @@ class Register extends Component {
                                     error={errors.name}
                                     id="name"
                                     type="text"
+                                    className={classnames("", {
+                                        invalid: errors.name
+                                    })}
                                 />
-                                <label htmlFor="name">Name</label>
+                                <label htmlFor="name">Nome</label>
+                                <span className="red-text">{errors.name}</span>
                             </div>
-                            <div class="input-field col s12">
-                                <select onChange={this.onChange} value={this.state.role}>
-                                    <option value="" disabled selected>Por favor, selecione</option>
-                                    <option value="Voluntário">Voluntário</option>
-                                    <option value="Entidade">Entidade</option>
-                                </select>
-                                <label>Role</label>
+
+                            <div className="input-field col s12">
+                                <input
+                                    onChange={this.onChange}
+                                    value={this.state.role}
+                                    error={errors.role}
+                                    id="role"
+                                    type="text"
+                                    className={classnames("", {
+                                        invalid: errors.role
+                                    })}
+                                />
+                                <label htmlFor="role">Role</label>
+                                <span className="red-text">{errors.role}</span>
                             </div>
+                            
                             <div className="input-field col s12">
                                 <input
                                     onChange={this.onChange}
@@ -89,9 +129,14 @@ class Register extends Component {
                                     error={errors.email}
                                     id="email"
                                     type="email"
+                                    className={classnames("", {
+                                        invalid: errors.email
+                                    })}
                                 />
                                 <label htmlFor="email">Email</label>
+                                <span className="red-text">{errors.email}</span>
                             </div>
+                            
                             <div className="input-field col s12">
                                 <input
                                     onChange={this.onChange}
@@ -99,9 +144,14 @@ class Register extends Component {
                                     error={errors.password}
                                     id="password"
                                     type="password"
+                                    className={classnames("", {
+                                        invalid: errors.password
+                                    })}
                                 />
                                 <label htmlFor="password">Password</label>
+                                <span className="red-text">{errors.password}</span>
                             </div>
+                            
                             <div className="input-field col s12">
                                 <input
                                     onChange={this.onChange}
@@ -109,9 +159,14 @@ class Register extends Component {
                                     error={errors.password2}
                                     id="password2"
                                     type="password"
+                                    className={classnames("", {
+                                        invalid: errors.password2
+                                    })}
                                 />
                                 <label htmlFor="password2">Confirm Password</label>
+                                <span className="red-text">{errors.password2}</span>
                             </div>
+                            
                             <div className="col s12" style={{ paddingLeft: "11.250px" }}>
                                 <button
                                     style={{
@@ -133,4 +188,18 @@ class Register extends Component {
     }
 }
 
-export default Register;
+Register.propTypes = {
+    registerUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+});
+
+export default connect(
+    mapStateToProps,
+    { registerUser }
+)(withRouter(Register));

75366bc4329cb298cab0cc90ba03d37205fb238f
André Costa <58915478+AlaudeBasker@users.noreply.github.com>
2020-05-18 23:18:00 +0100

    mail

---
+++ Notifications/notif.js
@@ -0,0 +1,20 @@
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+//process.env.SENDGRID_API_KEY
+const sgMail = require('@sendgrid/mail');
+
+
+sgMail.setApiKey('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');
+const msg = {
+  to: '170221078@estudantes.ips.pt',
+  from: 'ipsvoluntariado@gmail.com',
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg).then(() => {
+    console.log('Message sent')
+}).catch((error) => {
+    console.log(error.response.body)
+    // console.log(error.response.body.errors[0].message)
+})

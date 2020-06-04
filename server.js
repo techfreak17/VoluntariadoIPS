70a4cb81e2f6aa95e03901282017ceb2113d18de
JorgeSilvafu <jorgesilva04.12@gmail.com>
2020-06-04 23:55:19 +0100

    test update

--- server.js
+++ server.js
@@ -43,4 +43,6 @@ app.use("/api/users", users);
 app.use("/api/projects", projects);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+var server = app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+module.exports = server;
--- test/Test.js
+++ test/Test.js
@@ -1,8 +1,20 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+var request = require('supertest');
+
+describe('loading express', function () {
+  var server;
+  beforeEach(function () {
+    server = require('../server');
   });
+  afterEach(function () {
+    server.close();
+  });
+  /*
+  it('404 everything else', function testPath(done) {
+    request(server)
+      .get('/foo/bar')
+      .expect(404, done);
+  });
+  */
+
+  
 });

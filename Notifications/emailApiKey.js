01b29f0a3da89ac682bde7f4f364c49360a2a1d7
César Augusto Fonseca Fontinha <techfreak.thecaff@gmail.com>
2020-06-08 23:37:35 +0100

    cleanup

--- Notifications/emailApiKey.js
+++
@@ -1,5 +0,0 @@
-const apiKey = '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';
-
-module.exports = {
-    apiKey: apiKey
-}
--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://dbuser:leafGreen@maincluster-ebs9z.gcp.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: "secret"
-};

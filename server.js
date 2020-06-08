cc6f01927382580cbe0a202d483ac4072630e8c5
César Augusto Fonseca Fontinha <techfreak.thecaff@gmail.com>
2020-06-08 15:42:05 +0100

    Revert "quick fix"
    This reverts commit c5ec991afb518ace6575a825b1b84f5764c15253.

--- server.js
+++ server.js
@@ -55,5 +55,5 @@ app.use("/api/admin", admin);
 app.use("/api/aaips", aaips);
 app.use("/api/notifications",notifications)
 
-const port = process.env.PORT || 3000; // process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));

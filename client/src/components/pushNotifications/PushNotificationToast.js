95f034ca73bbbe411d948520972a4f1e6536d5de
bernardo.fortaleza <bernardo.fortaleza@gmail.com>
2020-06-20 01:18:52 +0100

    Notificações ok

--- client/src/components/pushNotifications/PushNotificationToast.js
+++ client/src/components/pushNotifications/PushNotificationToast.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Axios from "axios";
+import axios from 'axios';
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
@@ -20,7 +20,7 @@ class PushNotificationToast extends React.Component {
     }
 
     componentDidMount() {
-        Axios.get('/api/notifications/listNotifications/' + this.props.auth.user.id)
+        axios.get('/api/notifications/listNotifications/' + this.props.auth.user.id)
             .then(response => {
                 this.setState({ msgs: response.data });
             })
@@ -28,7 +28,7 @@ class PushNotificationToast extends React.Component {
                 console.log(error);
             });
 
-        Axios.get('/api/notifications/listReadNotifications/' + this.props.auth.user.id)
+        axios.get('/api/notifications/listReadNotifications/' + this.props.auth.user.id)
             .then(response => {
                 this.setState({ readMsgs: response.data });
             })
@@ -38,7 +38,7 @@ class PushNotificationToast extends React.Component {
     }
 
     markRead(notif) {
-        Axios.get('/api/notifications/updateNotification/' + notif._id)
+        axios.get('/api/notifications/updateNotification/' + notif._id)
             .catch(function (error) {
                 console.log(error);
             });
@@ -47,7 +47,9 @@ class PushNotificationToast extends React.Component {
     notify = () => {
         const notif = this.state.msgs[this.state.position];
         if (notif) {
-            toast(notif.body, { position: "bottom-right",  onClose: () => this.markRead(notif) });
+            let myDate = new Date(notif.date);
+            myDate = myDate.toLocaleString();
+            toast(notif.body + " (" + myDate + ")", { position: "bottom-right", onClose: () => this.markRead(notif) });
             this.setState({
                 position: this.state.position + 1,
             });
@@ -60,7 +62,9 @@ class PushNotificationToast extends React.Component {
     notifyRead = () => {
         const notif = this.state.readMsgs[this.state.readPos];
         if (notif) {
-            toast(notif.body, { position: "bottom-right"});
+            let myDate = new Date(notif.date);
+            myDate = myDate.toLocaleString();
+            toast(notif.body + " (" + myDate + ")", { position: "bottom-right" });
             this.setState({ readPos: this.state.readPos + 1 });
         } else {
             toast('Não existem mais notificações', { position: "bottom-right" });
@@ -71,11 +75,21 @@ class PushNotificationToast extends React.Component {
 
     render() {
         return (
-                <ul id="nav-mobile" className="right" >
-                    <li><button onClick={this.notify} style={{ backgroundColor: "transparent", color: "white", border: "none" }}><i className="material-icons">email</i></button></li>
-                    <li><button onClick={this.notifyRead} style={{ backgroundColor: "transparent", color: "white", border: "none" }}><i className="material-icons">drafts</i></button></li>
-                    <ToastContainer autoClose={10000} style={{width:"auto"}} />
-                </ul>
+            <ul id="nav-mobile" className="right" >
+                {(() => {
+                    if (this.state.msgs.length === 0) {
+                        return (
+                            <li><button onClick={this.notify} style={{ backgroundColor: "transparent", color: "white", border: "none" }}><i className="material-icons">email</i></button></li>
+                        )
+                    }else{
+                        return (
+                            <li><button onClick={this.notify} style={{ backgroundColor: "transparent", color: "red", border: "none" }}><i className="material-icons">email</i></button></li>
+                        )
+                    }
+                })()}
+                <li><button onClick={this.notifyRead} style={{ backgroundColor: "transparent", color: "white", border: "none" }}><i className="material-icons">drafts</i></button></li>
+                <ToastContainer autoClose={10000} style={{ width: "auto" }} />
+            </ul>
         );
     }
 
